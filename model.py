b76b9ac2cc001fe4759e31360de53468b692fe4e
Jevin Fernando <jevin.fernando@ifsworld.com>
2020-03-31 17:12:39 +0530

    annual predict summarize error fix

--- model.py
+++ model.py
@@ -58,6 +58,7 @@ def get_prediction_results(predict_freq, duration):
   if is_annual == True:
     result.index = result['ds'] 
     result = result.resample('Y').sum()
+    result.reset_index(level=0, inplace=True)
 
   result['ds'] = result['ds'].astype('string')
   return result
